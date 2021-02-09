0b27a2dc81a6781419e9d0f24c8e7573e3c79e47
tsuyoshi-ueda-ri <tsuyoshi010303@gmail.com>
2021-02-09 11:51:18 +0900

    routing-Configuration
    ルーティング設定。
    トップ画面を作る。

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.render('hello.ejs');
+});
+
+app.get('/top',(req, res) => {
+  res.render('top.ejs');
+});
+
 app.listen(3000);
