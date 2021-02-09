5bf1f3b6f4d6706678adeb5a96346ae46bcf272f
上田　剛史 <uedatsuyoshi@uedatakeshinoMacBook-Air.local>
2021-02-09 17:53:35 +0900

    mysql-setting
    mysql導入

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '01080108tp',
+  database: 'list_app'
+});
 app.get('/',(req, res) => {
   res.render('top.ejs');
 });
